8ab37eb68fb5b726c5f82249860937d01e23fd52
adias <ana.dias@noesis.pt>
2014-06-25 17:35:44 +0100

    JUnit class

---
+++ src/MainJunit.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class MainJunit {
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		assertTrue("Hello World!",true);
+	}
+
+}
