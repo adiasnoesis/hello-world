8c2cbfba2ef8821ea7873d2b7c92b164f57a375f
adias <ana.dias@noesis.pt>
2014-06-27 14:45:22 +0100

    update Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
-    }
+	private String name = "";
+
+	   public String getName() 
+	   {
+	      return name;
+	   }
+
+	   public String getMessage() 
+	   {
+	      if (name == "") 
+	      {
+	         return "Hello!";
+	      }
+	      else 
+	      {
+	         return "Hello " + name + "!";
+	      }
+	   }
+
+	   public void setName(String name) 
+	   {
+	      this.name = name;
+	   }
 }
